48a424e8c751f2af30fbb2851e5bf4b9a7b4fbcc
jakubmajer@live.com <jakubmajer@live.com>
2025-05-10 20:56:01 +0200

    Enhance logging in main application flow and restructure go.mod dependencies

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -10,12 +10,29 @@ import (
 )
 
 func main() {
+	// Log start of program
+	log.Println("Starting program...")
+
+	// Attempt to create the DB adapter
+	log.Println("Connecting to database...")
 	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
 	if err != nil {
 		log.Fatalf("Failed to connect to database. Error: %v", err)
 	}
+	log.Println("Successfully connected to the database.")
 
+	// Create the application with the dbAdapter
+	log.Println("Creating application instance...")
 	application := api.NewApplication(dbAdapter)
+
+	// Create the grpc adapter
+	log.Println("Creating gRPC adapter...")
 	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+
+	// Log when gRPC server starts
+	log.Println("Starting gRPC server...")
 	grpcAdapter.Run()
+
+	// Log when the program finishes (in case it exits gracefully)
+	log.Println("Program has finished running.")
 }
--- order/go.mod
+++ order/go.mod
@@ -3,8 +3,14 @@ module github.com/JFMajer/microservices/order
 go 1.24.3
 
 require (
+	github.com/JFMajer/microservices-proto v0.1.1
+	google.golang.org/grpc v1.72.0
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.26.1
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/JFMajer/microservices-proto v0.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -12,8 +18,5 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/grpc v1.72.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gorm v1.26.1 // indirect
 )
